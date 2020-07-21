4c4579b27f0464d63b3291f2a3fc5b4a291f2f5a
howiemalowie <havard.notland@gmail.com>
2020-07-21 16:09:07 +0200

    Added greedy clustering method

---
+++ ClusterGeneration/GreedyClustering.py
@@ -0,0 +1,30 @@
+from ClusterGeneration.Cluster import Cluster
+from ClusterGeneration.ClusterGroup import ClusterGroup
+
+
+def greedy_clustering(link_mat, lmt, base):
+    clusterList = dict()
+    clusterID = 0
+    clustered = dict.fromkeys(link_mat, False)
+    clustered[base] = True
+    for k in reversed(list(link_mat[base].keys())):
+        if not clustered[k]:
+            elem_list = [base, k]
+            clustered[k] = True
+            sz = 1
+            for elm in link_mat[k].keys():
+                if sz >= lmt:
+                    break
+                if not clustered[elm]:
+                    if link_mat[k][elm] <= link_mat[k][base]:
+                        elem_list.append(elm)
+                        clustered[elm] = True
+                        sz += 1
+                    else:
+                        break
+
+            clusterList[str(clusterID)] = Cluster(str(clusterID), elem_list, base)
+            clusterID += 1
+    clusterGroup = ClusterGroup(link_mat, lmt, clusterList)
+    return clusterGroup
+
