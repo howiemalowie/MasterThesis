e28ae4404310798f6d7689ef62a4defe36dd05eb
howiemalowie <havard.notland@gmail.com>
2020-07-29 15:20:11 +0200

    Further Kmeans implementation

--- ClusterGeneration/KMeans.py
+++ ClusterGeneration/KMeans.py
@@ -1,13 +1,35 @@
-import random
-import numpy as np
+import math
 from ClusterGeneration.Cluster import Cluster
+from ClusterGeneration.ClusterGroup import ClusterGroup
 
 
-def kmeans(link_mat, k, lmt):
+def find_new_centroid(cluster, coord_dict, mean_lon, mean_lat):
+
+    min_dist = float("inf")
+    new_centroid = ""
+    for p in cluster.get_elements():
+        lon = coord_dict[p][0]
+        lat = coord_dict[p][1]
+        dist = euclid_dist(lon, lat, mean_lon, mean_lat)
+        if dist < min_dist:
+            new_centroid = p
+            min_dist = dist
+
+    return new_centroid
+
+
+def euclid_dist(x1, y1, x2, y2):
+
+    return math.sqrt(pow(x1 - x2) + pow(y1 - y2))
+
+
+def kmeans(link_mat, coord_dict, k, lmt):
 
     clusters = dict()
     cluster_id = 0
+    elementLookUp = dict.fromkeys(link_mat.keys(), "0")
 
+    # Randomly select k elements to be initial centroids
     lst = link_mat.keys()
     lst.shuffle()
     for i in range(k):
@@ -15,15 +37,36 @@ def kmeans(link_mat, k, lmt):
         clusters[str(cluster_id)] = Cluster(cluster_id, ['B', lst[i]], 'B', centroid)
         cluster_id += 1
 
-    for key in link_mat.keys():
-        distances = dict.fromkeys(link_mat, 0)
-        for c in clusters.keys():
-            centroid = clusters[c].get_centroid()
-            distances[c] = ((link_mat[key][centroid] + link_mat[centroid][key]) / 2)
+    done = False
+    while not done:
 
-        closest_centroid_key = min(distances, key=distances.get)
-        clusters[closest_centroid_key].add_element(key)
+        # Assign each point to cluster corresponding to the closest centroid
+        for key in link_mat.keys():
+            if key == 'B':
+                continue
 
-        #NEXT: update centroids
+            distances = dict.fromkeys(clusters.keys(), float("inf"))
+            for c in clusters.keys():
+                centroid = clusters[c].get_centroid()
+                distances[c] = ((link_mat[key][centroid] + link_mat[centroid][key]) / 2)
 
-    return 0
+            closest_centroid = min(distances, key=distances.get)
+            clusters[elementLookUp[key]].remove_element(key)
+            clusters[closest_centroid].add_element(key)
+            elementLookUp[key] = closest_centroid
+
+        # Update centroids. If no centroids change, end
+        done = True
+        for c in clusters.values():
+            mean_lat = sum([coord_dict[p][0] for p in c.get_elements()])/c.get_cluster_size()
+            mean_lon = sum([coord_dict[p][1] for p in c.get_elements()])/c.get_cluster_size()
+
+            new_centroid = find_new_centroid(c, coord_dict, mean_lon, mean_lat)
+            if new_centroid != c.get_centroid():
+                done = False
+                c.set_centroid(new_centroid)
+
+    # Create clusterGroup once all clusters are finalized
+    clusterGroup = ClusterGroup(link_mat, lmt, clusters)
+
+    return clusterGroup
